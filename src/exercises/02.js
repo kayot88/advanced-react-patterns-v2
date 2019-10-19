11aa47062a0909e178ea00a3cc33774edfef389e
Kayot88 <kayot88@gmail.com>
2019-10-19 15:41:54 +0300

    patterns

---
+++ src/exercises/02.js
@@ -0,0 +1,44 @@
+// Compound Components
+
+import React from 'react'
+import {Switch} from '../switch'
+
+class Toggle extends React.Component {
+  static On = ({on, children}) => (on ? children : null)
+
+  static Off = ({on, children}) => (on ? null : children)
+
+  static Button = ({on, toggle}) => {
+    return <Switch on={on} onClick={toggle} />
+  }
+
+  state = {on: false}
+  toggle = () =>
+    this.setState(
+      ({on}) => ({on: !on}),
+      () => this.props.onToggle(this.state.on),
+    )
+  render() {
+    return React.Children.map(this.props.children, childElement => {
+      return React.cloneElement(childElement, {
+        on: this.state.on,
+        toggle: this.toggle,
+      })
+    })
+  }
+}
+
+function Usage({
+  onToggle = (...args) => console.log('onToggle', ...args),
+}) {
+  return (
+    <Toggle onToggle={onToggle}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+      <Toggle.Button />
+    </Toggle>
+  )
+}
+Usage.title = 'Compound Components'
+
+export {Toggle, Usage as default}
